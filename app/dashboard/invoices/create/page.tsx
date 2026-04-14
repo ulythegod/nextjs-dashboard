5e18e7a5db915a25e70dcde4bc124cbfe7a20531
YSkorobogatova <YSkorobogatova@nota.tech>
2026-04-14 08:47:08 +0300

    next js learning. dashboard app creating

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+import { useActionState } from 'react';
+ 
+export default async function Page() {
+  const customers = await fetchCustomers();
+ 
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
